6ab177788e1ed6a3f1142a2862827271f83a692e
Mircea Ungureanu <mircea.ungureanu@codecomputerlove.com>
2019-04-10 13:47:15 +0100

    feat: finished "Loading CSS into our React Application"

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 render(<StorePicker/>, document.querySelector('#main'));
